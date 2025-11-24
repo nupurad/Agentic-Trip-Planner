709beec085f30d54a494f8b46ad255b2a68f1c50
Nupur Dashputre <nupurdashputre@Mac.lan>
2025-11-23 16:10:36 -0800

    code added for loading llm

---
+++ utils/model_loader.py
@@ -0,0 +1,43 @@
+import os
+from dotenv import load_dotenv
+from typing import Any, Literal, Optional, Any
+from pydantic import BaseModel, Field
+from utils.config_loader import load_config
+from langchain_groq import ChatGroq
+
+
+
+class ConfigLoader:
+    def __init__(self):
+        print(f"Loaded config....")
+        self.confige = load_config()
+    
+    def __getitem__(self, key):
+        return self.confige.get[key]
+
+class ModelLoader:
+    model_provider: Literal["groq"] = "groq"
+    config: Optional[ConfigLoader] = Field(default=None, exclude=True)
+
+
+    def model_post_init(self, __context: Any) -> None:
+        self.config = ConfigLoader()
+    
+
+class Config:
+    arbitrary_types_allowed = True
+
+def load_llm(self):
+    print("Loading LLM model...")
+    print("Loading model from provider: {self.model_provider}")
+    if self.model_provider == "groq":
+        print("Loading LLM from Groq...")
+        groq_api_key = os.getenv("GROQ_API_KEY")
+        model_name = self.config["llm"]["groq"]["model_name"]
+        llm = ChatGroq(model=model_name, api_key=groq_api_key)
+        
+    return llm
+
+
+
+
